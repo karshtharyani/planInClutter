75770a21323a4679328d0edd4974163a4da2f972
Karsh Tharyani <karsht8@gmail.com>
2018-03-16 18:43:47 -0400

    added isObstacle and obstacles in way

--- planInClutter.py
+++ planInClutter.py
@@ -1,6 +1,7 @@
 #! /usr/bin/python
 from __future__ import division
 from makeGrid import *
+import matplotlib.pylab as plt
 import math
 
 def planInClutter(tableMap, targetObject):
@@ -10,19 +11,40 @@ def updateObjectOnTable(tableMap, obj, newX, newY, newTheta):
 	obj.updatePose(newX, newY, newTheta)
 	tableMap.updateWorldMap(obj)
 
+def convertObjectToPolar(tableObject):
+	xTarget = tableObject[0]
+	yTarget = tableObject[1]
+	rTarget = math.sqrt(tableObject[0]**2 + tableObject[1]**2)
+	thetaTarget = math.atan(xTarget/yTarget) * 180 / math.pi
+	return rTarget, thetaTarget
+
+def isObstacle(rTarget, thetaTarget, rObj, thetaObj, thetaTolerance):
+	thetaUpper = thetaTarget + thetaTolerance	
+	thetaLower = thetaTarget - thetaTolerance	
+	if rObj < rTarget and thetaObj > thetaLower and thetaObj < thetaUpper:
+		return True
+	
+
 def obstaclesInWay(tagTarget, tableMap, thetaTolerance = 10):
 	targetObject = tableMap.worldMap[tagTarget]
-	xTarget = targetObject[0]
-	yTarget = targetObject[1]
-	rTarget = math.sqrt(targetObject[0]**2 + targetObject[1]**2)
-	thetaTarget = math.atan(xTarget/yTarget) * 180 / math.pi
+	rTarget, thetaTarget = convertObjectToPolar(targetObject)
+	obstacles = []
+	for tag in tableMap.worldMap.keys():
+		if tag == tagTarget:
+			continue
+		tableObj = tableMap.worldMap[tag]
+		rObj, thetaObj = convertObjectToPolar(tableObj)
+		if isObstacle(rTarget, thetaTarget, rObj, thetaObj, thetaTolerance):
+			obstacles.append(tag)
+	return obstacles
 
 if __name__ == "__main__":
-	soup = objectOnTable(30, 40)
-	soup1 = objectOnTable(10, 20)
-	soup2 = objectOnTable(30, 25)
-	spam = objectOnTable(40, 20, numpy.pi/2, shape = 'rectangle')
-	glass = objectOnTable(20, 30)
+	soup = objectOnTable(10, 10)
+	soup1 = objectOnTable(15, 20)
+	soup2 = objectOnTable(30, 35)
+	spam = objectOnTable(40, 40, numpy.pi/2, shape = 'rectangle')
+	glass = objectOnTable(35, 25)
 	tableMap = costMap([soup, soup1, soup2, spam, glass])
-	obstaclesInWay(0, tableMap) 
+	obstaclesInWay(2, tableMap) 
+	tableMap.visualize()
 			
