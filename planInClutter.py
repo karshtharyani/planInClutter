3cd7636cee50250761785a3d09309e08cfb10b9f
Karsh <ktharyan@andrew.cmu.edu>
2018-03-18 11:46:22 -0400

    started adding convolve and few mods in findOpenSpot

--- planInClutter.py
+++ planInClutter.py
@@ -4,11 +4,15 @@ from makeGrid import *
 import matplotlib.pylab as plt
 import math
 import numpy as np
+from collections import namedtuple
 
 def planInClutter(tableMap, targetObject):
 	tableMap.visualize()
 	obstacles = obstaclesInWay(targetObject.tag, tableMap) 
 	print "these tags are obstacles :", obstacles
+	tableMap.visualize()
+	for tag in obstacles:
+		newX, newY, newTheta = findOpenSpot(tag, tableMap)
 	tableMap.visualize()
 	
 def updateObjectOnTable(tableMap, obj, newX, newY, newTheta):
@@ -16,9 +20,9 @@ def updateObjectOnTable(tableMap, obj, newX, newY, newTheta):
 	tableMap.updateWorldMap(obj)
 
 def convertObjectToPolar(tableObject):
-	xTarget = tableObject[0]
-	yTarget = tableObject[1]
-	rTarget = math.sqrt(tableObject[0]**2 + tableObject[1]**2)
+	xTarget = tableObject.x
+	yTarget = tableObject.y
+	rTarget = math.sqrt(xTarget**2 + yTarget**2)
 	thetaTarget = math.atan(xTarget/yTarget) * 180 / math.pi
 	return rTarget, thetaTarget
 
@@ -28,8 +32,17 @@ def isObstacle(rTarget, thetaTarget, rObj, thetaObj, thetaTolerance):
 	if rObj < rTarget and thetaObj > thetaLower and thetaObj < thetaUpper:
 		return True
 def findOpenSpot(tag, tableMap):
-	pass
+	for tag in tableMap.worldMap.keys():
+		obj = tableMap.worldMap[tag]
+		filterSize = namedtuple('filterSize', ['r', 'y'], verbose = False)
+		filtersz = filterSize(int(obj.delGridX), int(obj.delGridY))
+		filter = numpy.ones(filtersz)
+		newX, newY = convolve(tableMap.matMap, filter)
+	return 0, 0, 0
 	
+def convolve(matMap, filter):
+	
+	return 0, 0
 def fillGridRegion(matMap, rTarget, thetaUpper, thetaLower):
 	thetaUpper = int(math.floor(thetaUpper))
 	thetaLower = int(math.floor(thetaLower))
