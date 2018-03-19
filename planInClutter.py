5b55d40d4a5d38b0e4c7414b032b9a170bbcb5b1
Karsh <ktharyan@andrew.cmu.edu>
2018-03-19 14:37:30 -0400

    did some good stuff

--- planInClutter.py
+++ planInClutter.py
@@ -14,6 +14,7 @@ def planInClutter(tableMap, targetObject):
 	tableMap.visualize()
 	for tag in obstacles:
 		newX, newY, newTheta = findOpenSpot(tag, tableMap)
+		print newX, newY, newTheta
 		updateObjectOnTable(tableMap, tableMap.worldMap[tag], newX, newY, newTheta)
 		tableMap.visualize()
 	
@@ -37,29 +38,32 @@ def findOpenSpot(tag, tableMap):
 	obj = tableMap.worldMap[tag]
 	filterSize = namedtuple('filterSize', ['r', 'c'], verbose = False)
 	filtersz = filterSize(int(2 * obj.delGridX), int(2 * obj.delGridY))
-	newX, newY = convolve(tableMap.matMap, filtersz)
+	initialX = int(numpy.ceil(obj.x))
+	newX, newY = convolve(tableMap.matMap, filtersz, initialX)
 	return newX, newY, 0
 	
-def convolve(matMap, filtersz):
+def convolve(matMap, filtersz, initialX):
 	outOfBounds = 0
 	newX = 0
 	newY = 0
-	for j in range(15, numpy.size(matMap, 0)):
-		for i in range(15, numpy.size(matMap, 1)):
+	for i in range(initialX, numpy.size(matMap, 0)):
+		for j in range(5, numpy.size(matMap, 1)):
 			rCorn1 = i - int(filtersz[0] / 2)
 			rCorn2 = i + int(filtersz[0] / 2) + 1
 			cCorn1 = j - int(filtersz[1] / 2)
 			cCorn2 = j + int(filtersz[1] / 2) + 1
-			#print (rCorn1, cCorn1)
-			#print (rCorn2, cCorn2)
+			print "Corner 1: "
+			print (rCorn1, cCorn1)
+			print "Corner 2: "
+			print (rCorn2, cCorn2)
 			roi = matMap[rCorn1:rCorn2, cCorn1:cCorn2];
-			if numpy.size(roi, 0) != 3 or numpy.size(roi, 1) != 3:
-				outOfBounds = outOfBounds + 1
-			else:
-				if roi.any >= 0:
-					newX = i
-					newY = j
-					return newX, newY
+			print roi
+			print 
+			roiTruth = roi >= 0
+			if roiTruth.all() == True:
+				newX = i
+				newY = j
+				return newX, newY
 
 def fillGridRegion(matMap, rTarget, thetaUpper, thetaLower):
 	thetaUpper = int(math.floor(thetaUpper))
@@ -71,8 +75,8 @@ def fillGridRegion(matMap, rTarget, thetaUpper, thetaLower):
 			j1 = int(math.floor(math.tan(theta) * i))
 			if i**2 + j**2 <= rTarget**2:
 				try:
-					matMap[j, i] = -10
-					matMap[j1, i] = -10
+					matMap[j, i] = -0.5
+					matMap[j1, i] = -0.5
 				except:
 					print "index not possible"
 
@@ -95,11 +99,11 @@ def obstaclesInWay(tagTarget, tableMap, thetaTolerance = 15):
 	return obstacles
 
 if __name__ == "__main__":
-	soup = objectOnTable(10, 10)
-	soup1 = objectOnTable(20, 20)
-	soup2 = objectOnTable(30, 30)
-	spam = objectOnTable(20, 40, numpy.pi/2, shape = 'rectangle')
-	glass = objectOnTable(35, 25)
+	soup = objectOnTable(20, 20)
+	soup1 = objectOnTable(10, 10)
+	soup2 = objectOnTable(15, 40)
+	spam = objectOnTable(20, 10, numpy.pi/2, shape = 'rectangle')
+	glass = objectOnTable(30, 20)
 	tableMap = costMap([soup, soup1, soup2, spam, glass])
 	planInClutter(tableMap, glass)
 			
