7e52174e722f84809855995174727bb3c7b089ac
tcl326 <ouyangjiahong13@gmail.com>
2018-04-22 01:07:10 -0400

    added failure catches and added call makingEnvironment.renew_env()

--- Planning.py
+++ Planning.py
@@ -25,7 +25,7 @@ class Planning(object):
                 # pdb.set_trace()
                 self.plan = self.planInClutter(self.tableMap, self.targetObject)
                 ch = raw_input("Planning Complete, continue?");
-                self.executePlan()
+                #self.executePlan()
 
         def clutterMap(self):
                 kinbodies = self.env.GetBodies()
@@ -54,16 +54,28 @@ class Planning(object):
                                 print tag
                                 for reachObject in self.bodyOfConcern:
                                         if int(reachObject.GetName()[-2:]) == tag:
-                                                self.reachForObject(reachObject)
-                                                self.graspObject(reachObject)
-                                                self.placeObject(reachObject, newX, newY)
+                                                ro = self.reachForObject(reachObject)
+                                                if not ro:
+                                                    return False
+						go = self.graspObject(reachObject)
+                                                if not go:
+                                                    return False
+                                                po = self.placeObject(reachObject, newX, newY)
+                                                if not po:
+                                                    return False
+                                                    
                         print "Now going for target"
                 for targetObject in self.bodyOfConcern:
                         if self.targetObject.tag == int(targetObject.GetName()[-2:]):
-                                self.reachForObject(targetObject)
-                                self.graspObject(targetObject)
+                                ro = self.reachForObject(targetObject)
+				if not ro:
+				    return False
+                                go = self.graspObject(targetObject)
+				if not go:
+				    return False
                                 self.goHome()
                                 self.robot.arm.hand.CloseHand(0)
+                makingEnvironment.renew_env(self.env)
                 return True
 
         def reachForObject(self, targetObject):
@@ -76,7 +88,7 @@ class Planning(object):
                     print "I got tomato_soup_can"
                     Tw_e =  np.array([[ 0., 0., 1., -0.025], # away from can, radially
                                    [1., 0., 0., 0],
-                                   [0., 1., 0., 0.03], # height from can
+                                   [0., 1., 0., 0.05], # height from can
                                    [0., 0., 0., 1.]])
                     Bw[2,:] = [0.0, 0.015]
                     Bw[5,:] = [-np.pi, np.pi]
@@ -109,11 +121,15 @@ class Planning(object):
                         plan = planner.OptimizeTrajectory(self.robot, plan)
                     except:
                         None
+                    if time.time() - timeStart > 30.0:
+                       print "Reach for object failed: exitting"
+                       return False
                 print "Time to Plan to Reach: ", time.time() - timeStart
+
                 ch = raw_input("Planning Complete. Execute path? (y/n)")
                 if(ch == 'y' or ch == 'Y'):
                     self.robot.ExecutePath(plan)
-
+                return True
 
         def graspObject(self, targetObject):
                 finger_link_inds = []
@@ -126,19 +142,20 @@ class Planning(object):
                 self.robot.arm.hand.CloseHand(1.2)
                 rospy.sleep(3)
                 self.robot.Grab(targetObject, grablink = grab_link, linkstoignore = finger_link_inds)
+                return True
                 #self.env.RemoveKinBody(targetObject)
                 #self.robot.arm.hand.CloseHand(0)
 
         def placeObject(self, targetObject, targetX = 0, targetY = 0):
                 #IPython.embed()
                 timeStart = time.time()
-                T0_w = np.array([[1, 0, 0, targetX + 0.03],
+                T0_w = np.array([[1, 0, 0, targetX + 0.11],
                                  [0, 1, 0, targetY],
                                  [0, 0, 1, 0.73],
                                  [0, 0, 0, 1]])
                 Tw_e =  np.array([[ 0., 0., 1., -0.025], # away from can, radially
                            [1., 0., 0., 0],
-                           [0., 1., 0., 0.03], # height from can
+                           [0., 1., 0., 0.04], # height from can
                            [0., 0., 0., 1.]])
                 Bw = np.zeros((6,2))
                 Bw[2,:] = [0.0, 0.03]
@@ -172,13 +189,17 @@ class Planning(object):
                         plan = planner.OptimizeTrajectory(self.robot, plan)
                     except:
                         None
+                    if time.time() - timeStart > 30.0:
+                       print "Place for object failed: exitting"
+                       return False
                 print "Time to Plan to Reach: ", time.time() - timeStart
                 ch = raw_input("Planning Complete. Execute path? (y/n)")
                 if(ch == 'y' or ch == 'Y'):
                     self.robot.ExecutePath(plan)
                 self.robot.ReleaseAllGrabbed()
-                self.robot.arm.hand.CloseHand(0.3)
+                self.robot.arm.hand.CloseHand(0.25)
                 rospy.sleep(3)
+                return True
 
         def goHome(self):
                 self.robot.arm.PlanToNamedConfiguration('home', execute = True)
@@ -191,6 +212,9 @@ def planning_env(env, robot):
         ObjectTypes = manip_config['testObjectTypes']
         tags = manip_config['tags']
 
+        robot.arm.hand.CloseHand(0)
+        robot.arm.PlanToNamedConfiguration('home', execute = True)
+        robot.arm.hand.CloseHand(0)
         makingEnvironment.addObjects(env, Objects, ObjectTypes, tags)
         makingEnvironment.addTable(env, robot)
         makingEnvironment.addConstraintBoxes(env, robot)
